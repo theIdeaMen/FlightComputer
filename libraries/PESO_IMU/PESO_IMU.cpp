43e6b74fd6b29ae4a11a6058f75f8a2f10db9804
Griffin Adams <duff333@gmail.com>
2013-07-11 13:44:53 -0500

    Added more debug messages

--- libraries/PESO_IMU/PESO_IMU.cpp
+++ libraries/PESO_IMU/PESO_IMU.cpp
@@ -7,20 +7,30 @@ IMU::IMU()
 
 void IMU::initialize(void (*func)())
 {
-  mpu_init();
-  mpu_set_sensors(INV_XYZ_GYRO | INV_XYZ_ACCEL);
-  mpu_set_sample_rate(100);
-  mpu_set_accel_fsr(8);
-  Serial.println("Initialize");
+  Serial.print("\nmpu_init: ");
+  Serial.print(mpu_init());
+  Serial.print("\nmpu_set_sensors: ");
+  Serial.print(mpu_set_sensors(INV_XYZ_GYRO | INV_XYZ_ACCEL));
+  Serial.print("\nmpu_set_sample_rate: ");
+  Serial.print(mpu_set_sample_rate(100));
+  Serial.print("\nmpu_set_accel_fsr: ");
+  Serial.print(mpu_set_accel_fsr(8));
 
-  if (dmp_load_motion_driver_firmware() == 0)
+  int tmp = dmp_load_motion_driver_firmware();
+  Serial.print("\ndmp_load_motion_driver_firmware: ");
+  Serial.print(tmp);
+  if (tmp == 0)
   {
     //dmp_set_orientation(inv_orientation_matrix_to_scalar(gyro_orientation));
-    dmp_enable_feature(DMP_FEATURE_6X_LP_QUAT | DMP_FEATURE_SEND_RAW_ACCEL | DMP_FEATURE_SEND_CAL_GYRO | DMP_FEATURE_GYRO_CAL);
-    dmp_set_fifo_rate(100);
-    mpu_set_dmp_state(1);
+    Serial.print("\ndmp_enable_feature: ");
+    Serial.print(dmp_enable_feature(DMP_FEATURE_6X_LP_QUAT | DMP_FEATURE_SEND_RAW_ACCEL | DMP_FEATURE_SEND_CAL_GYRO | DMP_FEATURE_GYRO_CAL));
+    Serial.print("\ndmp_set_fifo_rate: ");
+    Serial.print(dmp_set_fifo_rate(100));
+    Serial.print("\nmpu_set_dmp_state: ");
+    Serial.print(mpu_set_dmp_state(1));
     attachInterrupt(0, func, RISING);
-    mpu_get_int_status(&mpuIntStatus);
+    Serial.print("\nmpu_get_int_status: ");
+    Serial.print(mpu_get_int_status(&mpuIntStatus));
     Serial.println("DMP On");
   }
 }
