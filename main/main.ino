528521e94c0c5aa1023ea8e8c4eaf98c26ec8f3c
Griffin Adams <duff333@gmail.com>
2013-12-30 21:36:05 -0600

    Started coding in main, added PESO_inc.h

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,46 @@
+#include <PESO_inc.h>
+
+// Objects
+Logger logger;    // Logs to microSD card over SPI
+IMU imu;          // Inertial measurement unit - MPU6050 breakout
+GPS gps;          // Global positioning system - Adafruit GPS breakout
+
+// Define threads
+ThreadController Controller = ThreadController();
+Thread IMU_thread = Thread();
+Thread GPS_thread = Thread();
+Thread COMMS_thread = Thread();
+Thread CUTDOWN_thread = Thread();
+
 void setup()
 {
-
-  /* add setup code here */
+  Serial.begin(115200);
+  
+  // Configure threads
+	IMU_thread.onRun(IMU_CB);
+	IMU_thread.setInterval(10);
+  
+  GPS_thread.onRun(GPS_CB);
+	GPS_thread.setInterval(100);
+  
+  COMMS_thread.onRun(COMMS_CB);
+	COMMS_thread.setInterval(50);
+  
+  CUTDOWN_thread.onRun(CUTDOWN_CB);
+	CUTDOWN_thread.setInterval(200);
+  
+  Controller.add(&IMU_thread);
+  Controller.add(&GPS_thread);
+  Controller.add(&COMMS_thread);
+  Controller.add(&CUTDOWN_thread);
 
 }
 
 void loop()
 {
-
-  /* add main program code here */
+  // Run threads
+  Controller.run();
+  
+  // Check for command from ground control
 
 }
